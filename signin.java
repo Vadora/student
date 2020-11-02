4a15d0428c6f0839e7d313dc8bc8da6da60fb391
io <io@qq.com>
2020-11-02 17:35:01 +0800

    解决冲突

--- signin.java
+++ signin.java
@@ -1,12 +1,9 @@
-<<<<<<< HEAD
 public class signin {
 	private int id;
 	private String name;
-=======
 public class signin{
-	private String name;
+	private String name1;
 	public void sign(){
 		System.out.println("sign name"+name);
 	}
->>>>>>> d1abea06910afd04908b2422598405dae0699c6f
 }
